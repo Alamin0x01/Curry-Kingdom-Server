0dd291537f7e81a23ad643cea53f45ced415719c
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-05-02 22:13:01 +0600

    all data in server

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const port = process.env.PORT || 8000;
+const cors = require("cors");
 const app = express();
+app.use(cors());
+const chefData = require("./data/chef.json");
+const recipesData = require("./data/recipes.json");
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello server running" });
 });
 
-// app.listen(9000, () => {
-//   console.log("server is running on port 5000");
-// });
+app.get("/chefData", (req, res) => {
+  res.send(chefData);
+});
+app.get("/recipesData", (req, res) => {
+  res.send(recipesData);
+});
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
