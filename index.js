0322950c6331c20d7447396e3a5cb837d20f9444
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-05-05 18:20:50 +0600

    slider items added

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const express = require("express");
 const port = process.env.PORT || 8000;
 const cors = require("cors");
 const app = express();
+const chefs = require("./data/chefs.json");
+const items = require("./data/items.json");
+
 app.use(cors());
-const chefData = require("./data/chef.json");
-const recipesData = require("./data/recipes.json");
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello server running" });
 });
 
-app.get("/chefData", (req, res) => {
-  res.send(chefData);
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
-app.get("/recipesData", (req, res) => {
-  res.send(recipesData);
+
+app.get("/chefs/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const specificDestination = chefs.find((spDes) => spDes.id == id) || {};
+  res.send(specificDestination);
 });
+
+app.get("/slides", (req, res) => {
+  res.send(items);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
